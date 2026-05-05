d59d8777ebe1bb8868cca97d3768a3ff2b5b4480
raza785612-cmd <raza785612@gmail.com>
2026-05-05 22:31:21 +0300

    add mission data

--- src/components/Station.jsx
+++ src/components/Station.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
-import { allMissionsData, STATION_PASSWORDS, groupsData } from './missionsData';
+import { allMissionsData, STATION_PASSWORDS, groupsData } from '../missionsData';
 
 
 
